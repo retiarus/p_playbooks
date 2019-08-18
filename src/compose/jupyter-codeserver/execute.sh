84c44a648ed2a2f39c876275c519879332595386
peregrinus <retiarus@gmail.com>
2019-08-18 18:57:00 -0300

    rename docker-files to compose and add new files

---
+++ src/compose/jupyter-codeserver/execute.sh
@@ -0,0 +1,1 @@
+docker run -p 8443:8443 -p 8888:8888 -v $(pwd)/data:/data -v $(pwd)/code:/code --rm -it example-ds-project
---
+++ src/compose/jupyterhub/jupyterhub_config.py
@@ -0,0 +1,6 @@
+import os
+
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_IMAGE']
+c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+c.JupyterHub.hub_ip = os.environ['HUB_IP']
